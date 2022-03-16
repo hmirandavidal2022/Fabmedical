6fb57148b8b819b5f0dfa83e8c0a45102ee18485
hmirandavidal2022 <hmirandavidal@epcor.com>
2022-03-16 19:13:35 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+
+appInsights.setup("3c9d7ca5-1c79-4dac-bc30-5d5f6a0db2bb");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
